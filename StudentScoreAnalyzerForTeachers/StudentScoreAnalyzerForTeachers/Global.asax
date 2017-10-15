61ce57d9d737c679c97c05ae6d4b74ccc5fbee76
thoma3mw <matthew.thomas0411@gmail.com>
2017-10-14 20:32:11 -0400

    Added initial solution

---
+++ StudentScoreAnalyzerForTeachers/StudentScoreAnalyzerForTeachers/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="StudentScoreAnalyzerForTeachers.MvcApplication" Language="C#" %>
---
+++ StudentScoreAnalyzerForTeachers/StudentScoreAnalyzerForTeachers/Startup.cs
@@ -0,0 +1,21 @@
+#region Copyright Syncfusion Inc. 2001-2017.
+// Copyright Syncfusion Inc. 2001-2017. All rights reserved.
+// Use of this code is subject to the terms of our license.
+// A copy of the current license can be obtained at any time by e-mailing
+// licensing@syncfusion.com. Any infringement will be prosecuted under
+// applicable laws. 
+#endregion
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(StudentScoreAnalyzerForTeachers.Startup))]
+namespace StudentScoreAnalyzerForTeachers
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
