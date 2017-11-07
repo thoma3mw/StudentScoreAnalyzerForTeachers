5ba383dcd3816082354d7354afd418df03aaed32
thoma3mw <matthew.thomas0411@gmail.com>
2017-11-06 23:13:53 -0500

    Updated a lot, path works through basic results

--- StudentScoreAnalyzerForTeachers/StudentScoreAnalyzerForTeachers/Controllers/HomeController.cs
+++ StudentScoreAnalyzerForTeachers/StudentScoreAnalyzerForTeachers/Controllers/HomeController.cs
@@ -9,6 +9,8 @@
 
 namespace StudentScoreAnalyzerForTeachers.Controllers
 {
+    using System.Collections.Generic;
+    using System.Linq;
     using System.Web.Mvc;
 
     using StudentScoreAnalyzerForTeachers.Models;
@@ -22,7 +24,9 @@ namespace StudentScoreAnalyzerForTeachers.Controllers
         /// <summary>
         /// Indexes this instance.
         /// </summary>
-        /// <returns>View</returns>
+        /// <returns>
+        /// View
+        /// </returns>
         public ActionResult Index()
         {
             return this.View(new NumberOfStudentsModel());
@@ -32,7 +36,9 @@ namespace StudentScoreAnalyzerForTeachers.Controllers
         /// Indexes the specified number of students model.
         /// </summary>
         /// <param name="numberOfStudentsModel">The number of students model.</param>
-        /// <returns>Action</returns>
+        /// <returns>
+        /// Action
+        /// </returns>
         [HttpPost]
         public ActionResult Index(NumberOfStudentsModel numberOfStudentsModel)
         {
@@ -48,7 +54,9 @@ namespace StudentScoreAnalyzerForTeachers.Controllers
         /// <summary>
         /// Abouts this instance.
         /// </summary>
-        /// <returns>View</returns>
+        /// <returns>
+        /// View
+        /// </returns>
         public ActionResult About()
         {
             this.ViewBag.Message = string.Empty;
@@ -59,7 +67,9 @@ namespace StudentScoreAnalyzerForTeachers.Controllers
         /// <summary>
         /// Contacts this instance.
         /// </summary>
-        /// <returns>View</returns>
+        /// <returns>
+        /// View
+        /// </returns>
         public ActionResult Contact()
         {
             this.ViewBag.Message = string.Empty;
@@ -76,8 +86,76 @@ namespace StudentScoreAnalyzerForTeachers.Controllers
         /// </returns>
         public ActionResult ScoreInput(int numberOfStudents)
         {
-            this.ViewBag.NumberOfStudents = numberOfStudents;
+            this.InitializeStudentScoreModels(numberOfStudents);
+
             return this.View();
+        }
+
+        /// <summary>
+        /// Batches the update.
+        /// </summary>
+        /// <param name="action">The action.</param>
+        /// <param name="added">The added.</param>
+        /// <param name="changed">The changed.</param>
+        /// <param name="deleted">The deleted.</param>
+        /// <param name="key">The key.</param>
+        /// <returns>Results View</returns>
+        public ActionResult BatchUpdate(string action, List<StudentScoreModel> added, List<StudentScoreModel> changed, List<StudentScoreModel> deleted, int? key)
+        {
+            var studentNumbersChanged = changed.Select(c => c.StudentNumber).ToList();
+            var studentsChanged = changed.ToDictionary(x => x.StudentNumber, x => x.StudentScore);
+            var models = (IEnumerable<StudentScoreModel>)this.Session["StudentScoreModels"];
+
+            foreach (var model in models)
+            {
+                if (studentNumbersChanged.Contains(model.StudentNumber))
+                {
+                    model.StudentScore = studentsChanged[model.StudentNumber];
+                }
+            }
+
+            return this.Json(this.Session["StudentScoreModels"], JsonRequestBehavior.AllowGet);
+        }
+
+        /// <summary>
+        /// Scores the input.
+        /// </summary>
+        /// <returns>Action</returns>
+        [HttpPost]
+        public ActionResult ScoreInput()
+        {
+            return this.RedirectToAction("Results");
+        }
+
+        /// <summary>
+        /// Results the specified scores models.
+        /// </summary>
+        /// <returns>
+        /// View
+        /// </returns>
+        public ActionResult Results()
+        {
+            return this.View();
+        }
+
+        /// <summary>
+        /// Initializes the student score models.
+        /// </summary>
+        /// <param name="numberOfStudents">The number of students.</param>
+        private void InitializeStudentScoreModels(int numberOfStudents)
+        {
+            var data = new List<StudentScoreModel>();
+            for (var i = 0; i < numberOfStudents; i++)
+            {
+                data.Add(
+                    new StudentScoreModel
+                        {
+                            StudentNumber = i + 1,
+                            StudentScore = decimal.Zero
+                        });
+            }
+
+            this.Session["StudentScoreModels"] = data;
         }
     }
 }
---
+++ StudentScoreAnalyzerForTeachers/StudentScoreAnalyzerForTeachers/Models/StudentScoreModel.cs
@@ -0,0 +1,33 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="StudentScoreModel.cs" company="Does Not Apply">
+// Copyright (c) Matthew Thomas. All rights reserved.
+// </copyright>
+// <summary>
+//   StudentScoreModel.cs
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace StudentScoreAnalyzerForTeachers.Models
+{
+    /// <summary>
+    /// Class StudentScoreModel
+    /// </summary>
+    public class StudentScoreModel
+    {
+        /// <summary>
+        /// Gets or sets the student number.
+        /// </summary>
+        /// <value>
+        /// The student number.
+        /// </value>
+        public int StudentNumber { get; set; }
+
+        /// <summary>
+        /// Gets or sets the student score.
+        /// </summary>
+        /// <value>
+        /// The student score.
+        /// </value>
+        public decimal StudentScore { get; set; }
+    }
+}
